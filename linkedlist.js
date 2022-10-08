7720573ea67d3f5f5913d35eac15058d469b61c3
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-10-08 22:27:36 +0100

    implementing append,print,contain to linked list

--- linkedlist.js
+++ linkedlist.js
@@ -1,3 +1,51 @@
 class Node{
-    constructor
+    constructor(val){
+        this.val = val
+        this.next = null
+    }
 }
+
+
+class LinkedList{
+    constructor(){
+        this.head = null
+    }
+
+
+
+    append(val){
+        if(this.head === null){
+            this.head = new Node(val)
+        }else{
+            let curr = this.head
+            while(curr.next !== null){
+                curr = curr.next
+            }
+            curr.next = new Node(val)
+        }
+    }
+
+    print(){
+        let str = ''
+        let curr = this.head
+        while(curr !== null){
+            str += `${curr.val} ->`
+            curr = curr.next
+        }
+        console.log(str);
+    }
+
+
+
+}
+
+
+let list = new LinkedList()
+
+list.append('a')
+list.append('b')
+list.append(8)
+list.append(9)
+list.append('j5')
+
+list.print()
